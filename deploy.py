ee05655950b8bf4127731d35b4c27ccb4e186c9d
Hugo Ruscitti <hugoruscitti@gmail.com>
2012-01-25 17:04:45 -0300

    agregando colorama y unos mensajes al script deploy.

--- deploy.py
+++ deploy.py
@@ -1,11 +1,16 @@
 import os
+from colorama import init
+from colorama import Fore, Back, Style
 
 import app
 from models import Note
 from config import DATABASE
 
+init(autoreset=True)
+
 auth = app.auth
 if os.path.exists(DATABASE['name']):
+    print Fore.GREEN  + 'Creando el archivo ' + DATABASE['name']
     os.remove(DATABASE['name'])
 
 #Crear tablas
@@ -16,3 +21,4 @@ Note.create_table(fail_silently=True)
 admin = auth.User(username='admin', admin=True, active=True)
 admin.set_password('admin')
 admin.save()
+print Fore.GREEN + "Creando usuario admin"
