26908f234157c99dee55f8a437a923dcc83fead1
Hugo Ruscitti <hugoruscitti@gmail.com>
2012-01-26 17:18:35 -0300

    creando una plantilla basica.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_peewee.auth import Auth
 from flask_peewee.db import Database
 from flask_peewee.admin import Admin
+from flask import render_template
 
 
 app = Flask(__name__)
@@ -21,7 +22,7 @@ def admin_setup():
 
 @app.route("/")
 def homepage():
-    return "<a href='/admin'>admin</a>"
+    return render_template('homepage.html')
 
 if __name__ == "__main__":
     admin_setup()
