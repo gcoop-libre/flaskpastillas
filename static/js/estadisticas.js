13a2df60a438f4d8b07eb7eb8818666f860287ab
Hugo Ruscitti <hugoruscitti@gmail.com>
2012-01-27 18:47:48 -0300

    creando un grafico estadistico de las llamadas.

---
+++ static/js/estadisticas.js
@@ -0,0 +1,41 @@
+var chart;
+
+$(document).ready(function() {
+   var datos= [
+                ['asdasd', 10],
+                ['123asdasd', 10],
+            ];
+   chart = new Highcharts.Chart({
+      chart: {
+         renderTo: 'container',
+         plotBackgroundColor: null,
+         plotBorderWidth: null,
+         plotShadow: false
+      },
+      title: {
+         text: 'Llamadas realizadas una o mas veces',
+      },
+      tooltip: {
+         formatter: function() {
+            return '<b>'+ this.point.name +'</b>: '+ this.percentage +' %';
+         }
+      },
+      plotOptions: {
+         pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+               enabled: true,
+               formatter: function() {
+                  return '<b>'+ this.point.name +'</b>: '+ this.percentage +' %';
+               }
+            }
+         }
+      },
+       series: [{
+         type: 'pie',
+         name: 'Browser share',
+         data: datos,
+      }]
+   });
+});
