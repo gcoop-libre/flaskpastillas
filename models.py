85117861e3ee936a947bf4240310d1116d408b48
Hugo Ruscitti <hugoruscitti@gmail.com>
2012-02-24 18:05:35 -0300

    generalizando el manejo de forms desde el modelo con el metodo 'cargar'

--- models.py
+++ models.py
@@ -2,8 +2,42 @@ import datetime
 from peewee import *
 
 from app import db
+from utils import consola
 
-class DatosBase(db.Model):
+class Modelo(db.Model):
+    """Representa un modelo que se puede cargar desde un formulario.
+    
+    Este método va a tratar de usar la intefaz del modelo para
+    asociar los campos. Así que si tu formulario es complejo
+    y tiene que tratar los datos antes de procesarlos, podría
+    utilizar properties.
+
+    Por ejemplo, si el formulario tiene un campo llamado 'csrf' podemos
+    interceptarlo con la siguiente property::
+
+        class MiModelo(Modelo):
+
+            def get_csrf(self):
+                pass
+
+            def set_csrf(self, valor):
+                print "Se quiere asignar el valor:", valor
+                pass
+
+            csrf = property(get_csrf, set_csrf)
+    
+    
+    """
+
+    def cargar(self, form):
+        datos = form.data
+
+        for (clave, valor) in datos.items():
+            setattr(self, clave, valor)
+
+        self.save()
+
+class DatosBase(Modelo):
     fecha = DateTimeField(default=datetime.datetime.now)
     telefono = CharField()
     nombre = CharField()
@@ -12,12 +46,9 @@ class DatosBase(db.Model):
     def __unicode__(self):
         return self.nombre
 
-    def cargar(self, form):
-        datos = form.data
-        self.nombre = datos['nombre']
-        self.edad = datos['edad']
-        self.save()
 
+"""
 class AbortoAnterior(db.Model):
     tipo = CharField()
     observaciones = TextField()
+"""
