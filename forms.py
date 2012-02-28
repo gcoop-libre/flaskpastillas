0d7e9e81581a5e74c5632f075874e1052612deeb
Hugo Ruscitti <hugoruscitti@gmail.com>
2012-02-28 16:03:13 -0300

    merge.

--- forms.py
+++ forms.py
@@ -6,18 +6,27 @@ import data
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
 
+
+class Separador(TextField):
+
+    def __init__(self, *k, **kv):
+        self.titulo = kv.pop('titulo')
+        TextField.__init__(self, *k, **kv)
+
+    def __call__(self):
+        return "<strong>%s</strong>" %(self.titulo)
+
 class DatosBaseForm(Form):
     nombre = TextField("Nombre")
     edad = DecimalField("Edad")
     telefono = TextField("Telefono")
-
-    provincia = ModelSelectField("Provincia", model=models.Provincia)
-    localidad = TextField("Localidad")
-
-    barrio = SelectField("Barrio", choices=data.BARRIOS, coerce=int)
     nacionalidad = TextField("Nacionalidad")
 
-    fum = DateField("FUM")
+    separador = Separador("", titulo="Direccion")
+    provincia = ModelSelectField("Provincia", model=models.Provincia)
+    localidad = TextField("Localidad")
+    barrio = SelectField("Barrio", choices=data.BARRIOS, coerce=int)
+    #fum = DateField("Fum", format='%d/%m/%Y')
 
     #canciones = FieldList(FormField(CancionForm))
     #submit = SubmitField("Agregar")
